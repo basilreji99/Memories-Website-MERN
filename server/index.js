a923a3b857cdcf3ee7d37b57749e49a33be348cf
Basil Reji <basil@Basil_Reji>
2024-03-03 17:46:41 -0500

    Initial Commit- Server and Client Side started. MongoDB Cluster Connected

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+
+const CONNECTION_URL = 'mongodb+srv://basilreji99:ev15YPUsAgPhknDI@cluster0.ntftgy8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewURLParser: true, useUnifiedTopology: true} )
+.then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
